9d5296a2c4e27e57e060a3ee5d6d664669271f59
Kanha <janmajaybagha@gamil.com>
2025-09-26 20:00:28 +0530

    Adding backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({ baseURL: 'https://chatapp-k15o.onrender.com/api' });
 
 export function setToken(token){
   if(token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
